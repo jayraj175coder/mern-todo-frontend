909b50986fd05c40b477e11c258e6498c115f390
jayraj175coder <jayrajsanas175@gmail.com>
2024-07-30 11:24:27 +0530

    addDeploy

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
-import './App';
+import './App.css';
 
-axios.defaults.baseURL = 'http://localhost:5000';
+//axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL ='https://mern-todo-backend-tjps.onrender.com';
 
 ReactDOM.render(
   <React.StrictMode>
